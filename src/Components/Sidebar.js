15bbec778b55afa1b0e1d016cbbca285f780b08d
Sasi367 <sasikumari0604@gmail.com>
2025-01-21 18:58:03 +0530

    Sidebar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,60 +1,30 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components'
 import { useGlobalContext } from '../context/global';
-
+import './Sidebar.css';
 
 function Sidebar() {
-    const {popularAnime} = useGlobalContext()
+    const { popularAnime } = useGlobalContext();
 
-    const sorted = popularAnime?.sort((a,b) => {
-        return b.score - a.score
-    })
+    const sorted = popularAnime?.sort((a, b) => {
+        return b.score - a.score;
+    });
 
     return (
-        <SidebarStyled>
+        <div className="sidebar">
             <h3>Top 5 Popular</h3>
             <div className="anime">
-                {sorted?.slice(0,5).map((anime) => {
-                    return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
-                        <img src={anime.images.jpg.large_image_url} alt="" />
-                        <h5>
-                            {anime.title}
-                        </h5>
-                    </Link>
+                {sorted?.slice(0, 5).map((anime) => {
+                    return (
+                        <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                            <img src={anime.images.jpg.large_image_url} alt="" />
+                            <h5>{anime.title}</h5>
+                        </Link>
+                    );
                 })}
             </div>
-        </SidebarStyled>
-    )
+        </div>
+    );
 }
 
-const SidebarStyled = styled.div`
-    margin-top: 2rem;
-    background-color: Black;
-    border-top: 5px solid #e5e7eb;
-    padding-right: 5rem;
-    padding-left: 2rem;
-    padding-top: 2rem;
-    .anime{
-        display: flex;
-        flex-direction: column;
-        width: 150px;
-        img{
-            width: 100%;
-            border-radius: 5px;
-            border: 5px solid #e5e7eb;
-        }
-        a{
-            margin-top: 1rem;
-            display: flex;
-            flex-direction: column;
-            gap: .4rem;
-            color: #27AE60;
-            h4{
-                font-size: 1.1rem;
-            }
-        }
-    }
-`;
-
-export default Sidebar
+export default Sidebar;
