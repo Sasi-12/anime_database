bf73beed79c29a0ffa4957d16caca4c9272c78c9
Sasi367 <sasikumari0604@gmail.com>
2025-01-26 16:06:38 +0530

    commit

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,37 +1,50 @@
-// Popular.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useGlobalContext } from '../context/global';
 import Sidebar from './Sidebar';
 import './Popular.css';
 
-function Popular({ rendered }) {
+function Popular({ viewType }) {
     const { popularAnime, isSearch, searchResults } = useGlobalContext();
 
-    const conditionalRender = () => {
-        if (!isSearch && rendered === 'popular') {
-            return popularAnime?.map((anime) => {
-                return (
-                    <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
-                        <img src={anime.images.jpg.large_image_url} alt="" />
-                    </Link>
-                );
-            });
-        } else {
-            return searchResults?.map((anime) => {
-                return (
-                    <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
-                        <img src={anime.images.jpg.large_image_url} alt="" />
-                    </Link>
-                );
-            });
-        }
+    // Render popular anime
+    const renderPopularAnime = () => {
+        return popularAnime?.length > 0 ? (
+            popularAnime.map((anime) => (
+                <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                    <img
+                        src={anime.images.jpg.large_image_url}
+                        alt={`Cover art for ${anime.title}`}
+                    />
+                </Link>
+            ))
+        ) : (
+            <p>No popular anime available.</p>
+        );
+    };
+
+    // Render search results
+    const renderSearchResults = () => {
+        return searchResults?.length > 0 ? (
+            searchResults.map((anime) => (
+                <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                    <img
+                        src={anime.images.jpg.large_image_url}
+                        alt={`Cover art for ${anime.title}`}
+                    />
+                </Link>
+            ))
+        ) : (
+            <p>No search results found.</p>
+        );
     };
 
     return (
         <div className="popular-container">
             <div className="popular-anime">
-                {conditionalRender()}
+                {!isSearch && viewType === 'popular'
+                    ? renderPopularAnime()
+                    : renderSearchResults()}
             </div>
             <Sidebar />
         </div>
